d6e14f06c23fc5de321f7f6470e1848aa8f89438
Amélie Fournier <1965556@cll.qc.ca>
2020-09-03 15:38:33 -0400

    Commentaire

--- Lab1/frmLab1.Designer.cs
+++ Lab1/frmLab1.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnAffiche = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Lab1
+            // btnAffiche
+            // 
+            this.btnAffiche.Location = new System.Drawing.Point(336, 208);
+            this.btnAffiche.Name = "btnAffiche";
+            this.btnAffiche.Size = new System.Drawing.Size(135, 23);
+            this.btnAffiche.TabIndex = 0;
+            this.btnAffiche.Text = "btnAffiche";
+            this.btnAffiche.UseVisualStyleBackColor = true;
+            this.btnAffiche.Click += new System.EventHandler(this.btnAffiche_Click);
+            // 
+            // frmLab1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Lab1";
+            this.Controls.Add(this.btnAffiche);
+            this.Name = "frmLab1";
             this.Text = "Lab 1 Conversion - Amélie Fournier";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAffiche;
     }
 }
 
--- Lab1/frmLab1.cs
+++ Lab1/frmLab1.cs
@@ -16,5 +16,10 @@ namespace Lab1
         {
             InitializeComponent();
         }
+
+        private void btnAffiche_Click(object sender, EventArgs e)
+        {
+            //Test
+        }
     }
 }
